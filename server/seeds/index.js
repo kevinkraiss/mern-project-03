d45bc12abee74f17537c7c393cf692e502c81e85
Jesus M <marquez.jay444@gmail.com>
2023-02-13 18:16:21 -0600

    updates seeds.index.js

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -1,10 +1,17 @@
 // const fetch = require('node-fetch')
 const { type } = require('os')
 const connection = require('../config/connection')
-const {User} = require('../models')
+const {User, Place} = require('../models')
 
 connection.once('open', async () => {
     await User.deleteMany()
+    await Place.deleteMany()
+    // seed pokemon
+    await Place.create({
+        name: 'Test' ,
+        formatted_address: '1234 W TEST ST 01234, TEST-CITY, TEST-STATE',
+        editorial_summary: 'BIRFOJRF PORFJRG[RWEP[FIPRHRVJPOE.IURHFIORGHPRI PGJPEROJGRF, IORNGIR OENGEERING ORIRNG, EFKFN LWIFIWR 1213  GGINRGNR. RIGNOPERGN GPOERJGER PIOGHIF.PROGJ ORIGPRJPGORG.FIJOPIRJGPOJRWEG.OEIJORP.ORG  JREGPO IORGR RIHGH ',
+    })
     await User.create({
         username: 'user',
         email: 'test@gmail.com',
