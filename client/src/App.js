406878766c6ca7c45a0c89e37dc94b07cff84044
Jesus M <marquez.jay444@gmail.com>
2023-02-14 14:59:20 -0600

    installs necessary packages, cleans up file structure, sets up apollo client in app.js, begins setting up structure for router and page components

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import Auth from './utils/auth'
+
+import Home from './pages/home'
+import User from './pages/user'
+import Login from './pages/login'
+
+// create and import components
+ 
+const httpLink = createHttpLink({
+  uri: '/graphql'
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token =  Auth.getToken()
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
     <ChakraProvider>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+
+    </ApolloProvider>
     </ChakraProvider>
   );
 }
