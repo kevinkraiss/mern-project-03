1c92df81a77f3dba731403e4da064bf7aa04121b
Ryan Jackson <rcjackson96@gmail.com>
2023-02-15 18:40:05 -0600

    started react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+<<<<<<< Updated upstream
+=======
+import { ChakraProvider } from '@chakra-ui/react';
+import './components/header';
+>>>>>>> Stashed changes
 
 function App() {
   return (
@@ -7,7 +12,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
