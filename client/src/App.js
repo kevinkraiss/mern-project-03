83598961fd1e5c2c63e2be40da2785ac97c30756
Snow Cactus <camdenz2k3@gmail.com>
2023-02-15 19:05:45 -0600

    fixing errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
+import React from 'react' 
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
 import Auth from './utils/auth'
 
+import Signup from './pages/signup'
 import Home from './pages/home'
 import User from './pages/user'
 import Login from './pages/login'
@@ -34,7 +36,36 @@ function App() {
   return (
     <ChakraProvider>
     <ApolloProvider client={client}>
-
+    <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />}
+              />
+              <Route 
+                path="/login" 
+                element={<Login />}
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />}
+              />
+              <Route 
+                path="/me" 
+                element={<User />}
+              />
+              <Route 
+                path="/profiles/:profileId"
+                element={<Profile />}
+              />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
     </ApolloProvider>
     </ChakraProvider>
   );
