c2ad09213978839d6c477239e9245b262e46cdcd
Brian Bulowicz <bajebi.bb@gmail.com>
2023-02-20 00:12:06 -0600

    maps and button text change

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -7,60 +7,28 @@ import oshMap2 from '../assets/oshkosh-map-2.jpg'
     
    
 function Maps(){
-    // const[map, setMap]=useState(`(${oshMap1})`)
-     const[map, setMap]=useState('popular')
+   
+     const[map, setMap]=useState('RECENT VISITS')
+     const[buttonText, setButtonText]=useState('SEE RECOMMENDED')
 
-    //     const dataSource = [
-    //         \client\src\assets\oshkosh-map-1.jpg
-    
-    //         client\src\assets\oshkosh-map-2.jpg
-    
-    //         /////OR???
-    //         {oshMap1} {oshMap2}
-    // ];
-    // const [allMaps]=useState([
+     const handleClick=event=>{ 
+        if(map==='RECENT VISITS'){
+            setMap('RECOMMENDED')
+            setButtonText('SEE RECENT VISITS')
+            }else{
+            setMap('RECENT VISITS')
+            setButtonText('SEE RECOMMENDED')
+            }
+           }
 
-    //     {
-    //         id:0,
-    //         mapName: "Oshkosh Places Visited ",
-    //         image: oshMap1,
-    //         alt:"city map of oshkosh",
-    //         text:"ESTABLISMENTS YOU'VE VISITED",
-    //         },
-    //     {
-    //         id:1,
-    //         appName: "Oshkosh places recommended",
-    //         image: oshMap2,
-    //         alt:"city map of oshkosh",
-    //         text:"ESTABLISHMENTS RECOMMENDED FOR YOU",
-    //        },
-    //     ])
-        
-        const handleClick=event=>{ 
-                if(map==='popular'){
-                    setMap('recommended')
-                }else{
-                    setMap('popular')
-                }
-            {console.log('clicked')}
-            // let file=event.target.files[0]
-            // setMap(`(${oshMap2})`)
-
-        }
         
      return (
     <div className="card">
             <div className="card-header">VITTLES OSHKOSH</div>
                 {map}
-            <img src={map==='popular' ? oshMap2 : oshMap1} className="map" style={{height:"250px", width:"250px"}} />
-                {/* <img src={map} className="map" style={{height:"250px", width:"250px"}} /> */}
+            <img src={map==='RECOMMENDED' ? oshMap2 : oshMap1} className="map" style={{height:"400px", width:"400px"}} />
            
-                {/* <div className="image" style={{backgroundImage:`url(${oshMap1})`}}></div> */}
-                {/* <img map></img> */}
-
-
-
-            <button onClick={handleClick}>CHANGE MAP VIEW</button>
+            <button style={{border:"solid",borderRadius:"10px"}}onClick={handleClick}>{buttonText}</button>
   
     </div>
 
