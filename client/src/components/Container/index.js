6190400fbcc3bf3f29546aedd11b9ce605738f4b
Ryan Jackson <rcjackson96@gmail.com>
2023-02-22 20:59:52 -0600

    restoring components

---
+++ client/src/components/Container/index.js
@@ -0,0 +1,9 @@
+const Container = props => {
+    return(
+        <div className={`container ${props.className || ''}`}>
+            {props.children}
+        </div>
+    )
+}
+
+export default Container
