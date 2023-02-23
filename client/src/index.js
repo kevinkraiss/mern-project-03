f3010a32b877fd21e79d7920ce46d8859f3a89cd
Jesus M <marquez.jay444@gmail.com>
2023-02-23 17:30:19 -0600

    attempting to fix service worker

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import { Workbox } from 'workbox-window'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -10,6 +11,13 @@ root.render(
     <App />
   </React.StrictMode>
 );
+if ('serviceWorker' in navigator) {
+  // registers workbox service worker
+  const workboxSW = new Workbox('/service-worker.js');
+  workboxSW.register();
+} else {
+  console.error('Service workers are not supported in this browser.');
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
